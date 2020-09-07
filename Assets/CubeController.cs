e6f8169f64889b6a1974b2db2bfb44d162f44e2f
Yulice-0603 <69956727+Yulice-0603@users.noreply.github.com>
2020-09-07 15:46:15 +0900

    Initial commit

---
+++ Assets/CubeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    AudioSource block;
+    //キューブの移動速度
+    float speed = -12;
+    //消滅位置
+    float deedLine = -10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        block = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+        //画面外に出たら破棄する
+        if (transform.position.x < this.deedLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "CubePrefab" || other.gameObject.tag == "ground")
+        {
+            block.Play();
+        }
+            
+    }
+}
